76125662bf935fbe37e0ca8df711a19fc1ea4fdf
FearlessSpiff <fearless.spiff@me.com>
2024-02-11 09:06:44 +0100

    add starting test config

---
+++ scripts/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rsync -av ~/.config/gBar ../.config
+rsync -av ~/.config/alacritty ../.config
+rsync -av ~/.config/wleave ../.config
+rsync -av ~/.config/rofi ../.config
+rsync -av ~/.config/hypr ../.config
