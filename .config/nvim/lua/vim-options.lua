889f6fe85b8d18c73f54b99c7df4013902e2cf09
FearlessSpiff <fearless.spiff@me.com>
2024-02-19 11:18:43 +0100

    add multi cursor plugin almost as in IDEA and let backus.sh delete old files

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -2,6 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- visual-multi plugin in multicursor.lua
+vim.cmd("let g:VM_maps = {}")
+vim.cmd("let g:VM_maps['Find Under']         = '<C-j>'")
+
 vim.g.mapleader = " "
 
 
--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rsync -av ~/.config/gBar ../.config
-rsync -av ~/.config/alacritty ../.config
-rsync -av ~/.config/wleave ../.config
-rsync -av ~/.config/rofi ../.config
-rsync -av ~/.config/hypr ../.config
-rsync -av ~/.config/nvim ../.config
+rsync -av --del ~/.config/gBar ../.config
+rsync -av --del ~/.config/alacritty ../.config
+rsync -av --del ~/.config/wleave ../.config
+rsync -av --del ~/.config/rofi ../.config
+rsync -av --del ~/.config/hypr ../.config
+rsync -av --del ~/.config/nvim ../.config
