2ce2a2c0a91e2970c433fc206f45f33cc3dae043
FearlessSpiff <fearless.spiff@me.com>
2024-02-19 14:57:48 +0100

    add git plugins

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,6 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<leader>tree', ':Neotree', {})
+    vim.keymap.set('n', '<leader>tree', ':Neotree<CR>', {})
   end
 }
